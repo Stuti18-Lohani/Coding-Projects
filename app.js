fc06d9f98e2a9ff3580ad64610e9c08f3abbe3ba
Stuti Lohani <stutilohani854@gmail.com>
2025-02-12 19:00:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const burgerImages =document.querySelectorAll(".burger img");
+const toggleImages = document.querySelectorAll(".burger-toggle img");
+let currentIndex=0;
+
+toggleImages[currentIndex].classList.add("active");
+
+burgerImages.forEach((img, index) =>{
+    img.addEventListener("click",() =>{
+        toggleImages.forEach(toggleImage => toggleImage.classList.remove("active"));
+        toggleImages[currentIndex].classList.add("active");
+        toggleImages[currentIndex].src= burgerImages[index].src;
+        currentIndex=(currentIndex+1)%toggleImages.length;
+    });
+});
+
+
+
+
+//Generate random stars//
+function createStars(){
+    const starsContainer = document.getElementById("stars");
+    const windowWidth =window.innerWidth;
+    const windowHeight =window.innerHeight;
+
+    for(let i=0; i<100; i++){
+        const star =document.createElement("div")
+        star.className ="star"
+        star.style.left = `${Math.random()*windowWidth}px`;
+        star.style.top = `${Math.random()*windowHeight}px`;
+        starsContainer.appendChild(star);
+    }
+}
+
+//Call the createStars function when the page loads
+window.addEventListener("load",createStars);    
